6fad7e565d6b415d6f91c39f19002e4335e42fca
phanendravaka <phanendravaka@gmail.com>
2025-11-22 19:50:18 +0000

    Fullstack prints LAB5

--- app.js
+++ app.js
@@ -22,3 +22,7 @@ app.post('/products', api.createProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
+app.get('/orders', api.listOrders)
+app.post('/orders', api.createOrder)
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id', api.deleteOrder)
